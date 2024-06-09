3763b83d28324ad21bbc079213b1d15fff6c9c72
Sarah Myers <4229141+lainceline@users.noreply.github.com>
2024-06-09 02:42:15 -0400

    Implement api.js

--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,24 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.route('/api/convert')
+    .get(function (req, res){
+      let input = req.query.input;
+      let initNum = convertHandler.getNum(input);
+      let initUnit = convertHandler.getUnit(input);
+      if (initNum === 'invalid number' && initUnit === 'invalid unit') {
+        return res.json({error: 'invalid number and unit'});
+      } else if (initNum === 'invalid number') {
+        return res.json({error: 'invalid number'});
+      } else if (initUnit === 'invalid unit') {
+        return res.json({error: 'invalid unit'});
+      }
+      let returnNum = convertHandler.convert(initNum, initUnit);
+      let returnUnit = convertHandler.getReturnUnit(initUnit);
+      let string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+      res.json({initNum, initUnit, returnNum, returnUnit, string});
+    });
+
+  
+
 };
